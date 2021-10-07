bd92b8b983b567f53403285b013ac0c74ce3c69a
sabhas <sabirbk06@gmail.com>
2021-10-07 17:18:51 +0000

    chore: add configuration for static resources and rendering views

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import indexRouter from './routes'
-
+import path from 'path'
 const app = express()
 
 app.use(express.json({ limit: '50mb' }))
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use('/', indexRouter)
 
 export default app
