4d8efbb88d32154d84e80b79780e2e3de2f519e4
Yury Shkoda <shkoda.yuri@gmail.com>
2021-10-11 16:42:17 +0300

    fix(prod-server): use port from configuration

--- src/prod-server.ts
+++ src/prod-server.ts
@@ -1,10 +1,9 @@
 import path from 'path'
 import { readFileSync } from 'fs'
 import * as https from 'https'
-
+import { configuration } from '../package.json'
 import app from './app'
 
-const port = 5001
 const keyPath = path.join('certificates', 'privkey.pem')
 const certPath = path.join('certificates', 'fullchain.pem')
 
@@ -13,6 +12,8 @@ const cert = readFileSync(certPath)
 
 const httpsServer = https.createServer({ key, cert }, app)
 
-httpsServer.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+httpsServer.listen(configuration.sasJsPort, () => {
+  console.log(
+    `⚡️[server]: Server is running at https://localhost:${configuration.sasJsPort}`
+  )
 })
