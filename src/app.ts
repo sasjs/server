41d6e5354817c9144cf53347fc7f995568e3f0b4
sabhas <sabirbk06@gmail.com>
2021-10-14 18:17:08 +0000

    chore: remove unnecessary express middleware

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,7 @@ import path from 'path'
 const app = express()
 
 app.use(express.json({ limit: '50mb' }))
-
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-
-app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use('/', indexRouter)
+app.use(express.static(path.join(__dirname, '..', 'Web', 'build')))
 
 export default app
