7396f4c95201f8592b3c0fea2d739c28ca359bab
sabhas <sabirbk06@gmail.com>
2021-10-18 11:36:28 +0000

    chore: add base container for home page

---
+++ Web/src/components/home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
+
+const Home = () => {
+  return (
+    <Box sx={{ display: 'flex' }} className="main">
+      <CssBaseline />
+      <h2>this is home component</h2>
+    </Box>
+  )
+}
+
+export default Home
