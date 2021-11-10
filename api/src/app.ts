f81c4a4ab7d10fdf2cf730118e30cfbcfe53c4ee
sabhas <sabirbk06@gmail.com>
2021-11-10 16:40:42 +0000

    fix: cors issue resolved for localhost:3000

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,13 +4,15 @@ import apiRouter from './routes/api'
 import { getWebBuildFolderPath } from './utils'
 
 const app = express()
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  next()
+})
 
 app.use(express.json({ limit: '50mb' }))
-
+app.use(express.static(getWebBuildFolderPath()))
 app.use('/', webRouter)
 app.use('/SASjsApi', apiRouter)
-app.use(express.json({ limit: '50mb' }))
-
-app.use(express.static(getWebBuildFolderPath()))
 
 export default app
