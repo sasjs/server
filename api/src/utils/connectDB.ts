511a68732b9a3f2d80bdace7c20b2bb354423ee5
Saad Jutt <ihsan.distranger@gmail.com>
2021-11-30 04:14:54 +0500

    chore: sasjs/version bumped

--- api/src/utils/connectDB.ts
+++ api/src/utils/connectDB.ts
@@ -3,33 +3,30 @@ import mongoose from 'mongoose'
 import { configuration } from '../../package.json'
 import { getDesktopFields } from '.'
 import { populateClients } from '../routes/api/auth'
-import shelljs from 'shelljs'
 
 export const connectDB = async () => {
-  shelljs.exec(`sasjs v`)
-
-  const { MODE } = process.env
-  if (MODE?.trim() !== 'server') {
-    console.log('Running in Destop Mode, no DB to connect.')
-
-    const { sasLoc, driveLoc } = await getDesktopFields()
-
-    process.sasLoc = sasLoc
-    process.driveLoc = driveLoc
-
-    return
-  } else {
-    const { SAS_PATH } = process.env
-    const sasDir = SAS_PATH ?? configuration.sasPath
-
-    process.sasLoc = path.join(sasDir, 'sas')
-  }
-
-  console.log('sasLoc: ', process.sasLoc)
-
   // NOTE: when exporting app.js as agent for supertest
   // we should exlcude connecting to the real database
   if (process.env.NODE_ENV !== 'test') {
+    const { MODE } = process.env
+    if (MODE?.trim() !== 'server') {
+      console.log('Running in Destop Mode, no DB to connect.')
+
+      const { sasLoc, driveLoc } = await getDesktopFields()
+
+      process.sasLoc = sasLoc
+      process.driveLoc = driveLoc
+
+      return
+    } else {
+      const { SAS_PATH } = process.env
+      const sasDir = SAS_PATH ?? configuration.sasPath
+
+      process.sasLoc = path.join(sasDir, 'sas')
+    }
+
+    console.log('sasLoc: ', process.sasLoc)
+
     mongoose.connect(process.env.DB_CONNECT as string, async (err) => {
       if (err) throw err
 
