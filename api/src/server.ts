0cfe724ffa089b84a9f8bca49c9033b56f51c9cb
Saad Jutt <ihsan.distranger@gmail.com>
2022-02-14 19:12:37 +0500

    fix: release should also has https protocol

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,40 @@
+import path from 'path'
+import { createServer } from 'https'
+import { readFile } from '@sasjs/utils'
+
 import appPromise from './app'
 
-appPromise.then((app) => {
+appPromise.then(async (app) => {
+  const protocol = process.env.PROTOCOL ?? 'http'
   const sasJsPort = process.env.PORT ?? 5000
-  app.listen(sasJsPort, () => {
-    console.log(
-      `⚡️[server]: Server is running at http://localhost:${sasJsPort}`
-    )
-  })
+
+  if (protocol !== 'https') {
+    app.listen(sasJsPort, () => {
+      console.log(
+        `⚡️[server]: Server is running at http://localhost:${sasJsPort}`
+      )
+    })
+  } else {
+    const { key, cert } = await getCertificates()
+
+    const httpsServer = createServer({ key, cert }, app)
+    httpsServer.listen(sasJsPort, () => {
+      console.log(
+        `⚡️[server]: Server is running at https://localhost:${sasJsPort}`
+      )
+    })
+  }
 })
+
+const getCertificates = async () => {
+  const privkey = process.env.PRIVATE_KEY ?? 'privkey.pem'
+  const fullchain = process.env.FULL_CHAIN ?? 'fullchain.pem'
+
+  const keyPath = path.join(process.cwd(), privkey)
+  const certPath = path.join(process.cwd(), fullchain)
+
+  const key = await readFile(keyPath)
+  const cert = await readFile(certPath)
+
+  return { key, cert }
+}
