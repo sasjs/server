e60f17268d1fa9ab623313026d46bd3f63756f69
Saad Jutt <ihsan.distranger@gmail.com>
2022-02-28 04:06:13 +0500

    fix: multi-part file upload + validations + specs

--- api/src/routes/api/drive.ts
+++ api/src/routes/api/drive.ts
@@ -1,11 +1,13 @@
 import express from 'express'
+import multer, { multerSingle } from '../../middlewares/multer'
 import { DriveController } from '../../controllers/'
 import { getFileDriveValidation, updateFileDriveValidation } from '../../utils'
+
+const controller = new DriveController()
 
 const driveRouter = express.Router()
 
 driveRouter.post('/deploy', async (req, res) => {
-  const controller = new DriveController()
   try {
     const response = await controller.deploy(req.body)
     res.send(response)
@@ -22,7 +24,6 @@ driveRouter.get('/file', async (req, res) => {
   const { error, value: query } = getFileDriveValidation(req.query)
   if (error) return res.status(400).send(error.details[0].message)
 
-  const controller = new DriveController()
   try {
     const response = await controller.getFile(query.filePath)
     res.send(response)
@@ -35,28 +36,28 @@ driveRouter.get('/file', async (req, res) => {
   }
 })
 
-driveRouter.post('/file', async (req, res) => {
-  const { error, value: body } = updateFileDriveValidation(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
+driveRouter.post(
+  '/file',
+  (...arg) => multerSingle('file', arg),
+  async (req, res) => {
+    const { error, value: body } = updateFileDriveValidation(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
 
-  const controller = new DriveController()
-  try {
-    const response = await controller.saveFile(body)
-    res.send(response)
-  } catch (err: any) {
-    const statusCode = err.code
+    if (!req.file) return res.status(400).send('"file" is not present.')
 
-    delete err.code
-
-    res.status(statusCode).send(err)
+    try {
+      const response = await controller.saveFile(body.filePath, req.file)
+      res.send(response)
+    } catch (err: any) {
+      res.status(403).send(err.toString())
+    }
   }
-})
+)
 
 driveRouter.patch('/file', async (req, res) => {
   const { error, value: body } = updateFileDriveValidation(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
-  const controller = new DriveController()
   try {
     const response = await controller.updateFile(body)
     res.send(response)
@@ -70,7 +71,6 @@ driveRouter.patch('/file', async (req, res) => {
 })
 
 driveRouter.get('/fileTree', async (req, res) => {
-  const controller = new DriveController()
   try {
     const response = await controller.getFileTree()
     res.send(response)
