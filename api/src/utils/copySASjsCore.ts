9e53470947350f4b8d835a2cb6b70e3dabf247c4
Saad Jutt <ihsan.distranger@gmail.com>
2022-03-23 17:15:46 +0500

    fix: quick fix for executables

--- api/src/utils/copySASjsCore.ts
+++ api/src/utils/copySASjsCore.ts
@@ -1,10 +1,8 @@
-import { copy, createFolder } from '@sasjs/utils'
+import { copy } from '@sasjs/utils'
 
 import { getTmpMacrosPath, sasJSCoreMacros } from '.'
 
 export const copySASjsCore = async () => {
-  await createFolder(sasJSCoreMacros)
-
   const macrosDrivePath = getTmpMacrosPath()
   await copy(sasJSCoreMacros, macrosDrivePath)
 }
