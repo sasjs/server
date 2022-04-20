36628551ae577598568ebf7864ddee00a60f4a79
Sabir Hassan <sabirbk06@gmail.com>
2022-04-21 04:37:40 +0500

    chore(web): use AppContext instead of useTokens Hook

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import AppContextProvider from './context/appContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
