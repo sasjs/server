4a8e32dd20b540b6dc92d749fad90d6c7fc69376
Saad Jutt <ihsan.distranger@gmail.com>
2022-05-08 03:18:04 +0500

    fix: added more cookies to req

--- api/src/utils/getPreProgramVariables.ts
+++ api/src/utils/getPreProgramVariables.ts
@@ -6,12 +6,18 @@ export const getPreProgramVariables = (req: any): PreProgramVars => {
   const { user, accessToken } = req
   const csrfToken = req.headers['x-xsrf-token']
   const sessionId = req.cookies['connect.sid']
+  const { _csrf } = req.cookies
 
   const httpHeaders: string[] = []
 
   if (accessToken) httpHeaders.push(`Authorization: Bearer ${accessToken}`)
   if (csrfToken) httpHeaders.push(`x-xsrf-token: ${csrfToken}`)
-  if (sessionId) httpHeaders.push(`cookie: connect.sid=${sessionId}`)
+
+  const cookies: string[] = []
+  if (sessionId) cookies.push(`connect.sid=${sessionId}`)
+  if (_csrf) cookies.push(`_csrf=${_csrf}`)
+
+  if (cookies.length) httpHeaders.push(`cookie: ${cookies.join('; ')}`)
 
   return {
     username: user.username,
