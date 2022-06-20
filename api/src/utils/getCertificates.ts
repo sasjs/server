d1a02c0da535433a6d655f4ea5b0894260eb8718
Saad Jutt <ihsan.distranger@gmail.com>
2022-06-21 02:08:43 +0500

    chore: debugging certs

--- api/src/utils/getCertificates.ts
+++ api/src/utils/getCertificates.ts
@@ -2,18 +2,21 @@ import path from 'path'
 import { fileExists, getString, readFile } from '@sasjs/utils'
 
 export const getCertificates = async () => {
-  const { PRIVATE_KEY, FULL_CHAIN } = process.env
+  const { PRIVATE_KEY, FULL_CHAIN, CA } = process.env
 
   const keyPath = PRIVATE_KEY ?? (await getFileInput('Private Key (PEM)'))
   const certPath = FULL_CHAIN ?? (await getFileInput('Full Chain (PEM)'))
+  const caPath = CA ?? (await getFileInput('Full Chain (PEM)'))
 
   console.log('keyPath: ', keyPath)
   console.log('certPath: ', certPath)
+  console.log('caPath: ', caPath)
 
   const key = await readFile(keyPath)
   const cert = await readFile(certPath)
+  const ca = await readFile(caPath)
 
-  return { key, cert }
+  return { key, cert, ca }
 }
 
 const getFileInput = async (filename: string): Promise<string> => {
