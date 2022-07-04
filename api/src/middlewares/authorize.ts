e54a09db19ec8690e54a40760531a4e06d250974
Sabir Hassan <sabirbk06@gmail.com>
2022-07-04 17:14:17 +0500

    fix: add authorize middleware for appStreams

--- api/src/middlewares/authorize.ts
+++ api/src/middlewares/authorize.ts
@@ -5,7 +5,7 @@ import { PermissionSetting } from '../controllers/permission'
 
 export const authorize: RequestHandler = async (req, res, next) => {
   let permission
-  const user = req.user
+  const user = req.user || req.session.user
   if (user) {
     // no need to check for permissions when user is admin
     if (user.isAdmin) return next()
@@ -13,7 +13,7 @@ export const authorize: RequestHandler = async (req, res, next) => {
     const dbUser = await User.findOne({ id: user.userId })
     if (!dbUser) return res.sendStatus(401)
 
-    const uri = req.baseUrl + req.route.path
+    const uri = req.baseUrl + req.path
 
     // find permission w.r.t user
     permission = await Permission.findOne({ uri, user: dbUser._id })
