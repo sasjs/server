763360831810bb7868e73e58624d2113552d7d83
Mihajlo Medjedovic <mihajlo.medjedovic@outlook.com>
2022-08-31 13:31:28 +0200

    chore: mocker architecture fix, env validation

--- api/src/routes/web/index.ts
+++ api/src/routes/web/index.ts
@@ -1,8 +1,26 @@
 import express from 'express'
+import sas9WebRouter from './sas9-web'
+import sasViyaWebRouter from './sasviya-web'
 import webRouter from './web'
 
 const router = express.Router()
 
-router.use('/', webRouter)
+const { MOCK_SERVERTYPE } = process.env
+
+console.log('MOCK_SERVERTYPE', MOCK_SERVERTYPE)
+
+switch (MOCK_SERVERTYPE?.toUpperCase()) {
+  case 'SAS9': {
+    router.use('/', sas9WebRouter)
+    break
+  }
+  case 'SASVIYA': {
+    router.use('/', sasViyaWebRouter)
+    break
+  }
+  default: {
+    router.use('/', webRouter)
+  }
+}
 
 export default router
--- api/src/routes/web/web.ts
+++ api/src/routes/web/web.ts
@@ -2,12 +2,9 @@ import express from 'express'
 import { WebController } from '../../controllers/web'
 import { authenticateAccessToken, desktopRestrict } from '../../middlewares'
 import { authorizeValidation, loginWebValidation } from '../../utils'
-import mockSas9Router from '../api/mock-sas9'
 
 const webRouter = express.Router()
 const controller = new WebController()
-
-const { MOCK_SERVERTYPE } = process.env
 
 webRouter.get('/', async (req, res) => {
   let response
@@ -26,22 +23,17 @@ webRouter.get('/', async (req, res) => {
   }
 })
 
-/**
- * If any type of mock is enabled, we won't use regular working logon endpoints
- */
-if (MOCK_SERVERTYPE === undefined) {
-  webRouter.post('/SASLogon/login', desktopRestrict, async (req, res) => {
-    const { error, value: body } = loginWebValidation(req.body)
-    if (error) return res.status(400).send(error.details[0].message)
+webRouter.post('/SASLogon/login', desktopRestrict, async (req, res) => {
+  const { error, value: body } = loginWebValidation(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
-    try {
-      const response = await controller.login(req, body)
-      res.send(response)
-    } catch (err: any) {
-      res.status(403).send(err.toString())
-    }
-  })
-}
+  try {
+    const response = await controller.login(req, body)
+    res.send(response)
+  } catch (err: any) {
+    res.status(403).send(err.toString())
+  }
+})
 
 webRouter.post(
   '/SASLogon/authorize',
@@ -60,22 +52,13 @@ webRouter.post(
   }
 )
 
-/**
- * If any type of mock is enabled, we won't use regular working logon endpoints
- */
-if (MOCK_SERVERTYPE === undefined) {
-  webRouter.get('/SASLogon/logout', desktopRestrict, async (req, res) => {
-    try {
-      await controller.logout(req)
-      res.status(200).send('OK!')
-    } catch (err: any) {
-      res.status(403).send(err.toString())
-    }
-  })
-}
-
-webRouter.use('/', mockSas9Router)
-// disabled for now
-// webRouter.use('/', mockViyaRouter)
+webRouter.get('/SASLogon/logout', desktopRestrict, async (req, res) => {
+  try {
+    await controller.logout(req)
+    res.status(200).send('OK!')
+  } catch (err: any) {
+    res.status(403).send(err.toString())
+  }
+})
 
 export default webRouter
