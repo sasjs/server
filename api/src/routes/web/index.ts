76bf84316e4a63da2d5798facc16379a4d56b729
Saad Jutt <ihsan.distranger@gmail.com>
2022-09-01 23:34:57 +0500

    chore: MOCK_SERVERTYPE instead of string literals

--- api/src/routes/web/index.ts
+++ api/src/routes/web/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import sas9WebRouter from './sas9-web'
 import sasViyaWebRouter from './sasviya-web'
 import webRouter from './web'
+import { MOCK_SERVERTYPEType } from '../../utils'
 
 const router = express.Router()
 
@@ -9,12 +10,12 @@ const { MOCK_SERVERTYPE } = process.env
 
 console.log('MOCK_SERVERTYPE', MOCK_SERVERTYPE)
 
-switch (MOCK_SERVERTYPE?.toUpperCase()) {
-  case 'SAS9': {
+switch (MOCK_SERVERTYPE) {
+  case MOCK_SERVERTYPEType.SAS9: {
     router.use('/', sas9WebRouter)
     break
   }
-  case 'SASVIYA': {
+  case MOCK_SERVERTYPEType.SASVIYA: {
     router.use('/', sasViyaWebRouter)
     break
   }
